15f54313ad33a30ee63663b3af6f83649b169e53
Praveshika2k <praveshikaonly2k@gmail.com>
2025-09-03 20:35:38 +0530

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,6 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB Connected');
-    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+    app.listen(PORT, () => console.log(`Server running on ${PORT}`));
   })
   .catch(err => console.error(err));
